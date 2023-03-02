969c392990ded32fffce8a5819409eadab4ce71a
Diego Delgado <diegodelgado_96@hotmail.com>
2023-03-02 03:16:16 -0500

    validation of jwt

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -6,3 +6,11 @@ export const generateToken = (data: any) => {
     expiresIn: "24h",
   });
 };
+
+export const validateToken = (token?: string) => {
+  const decoded = jwt.verify(token!, process.env.JWT_KEY!, {
+    maxAge: '24h'
+  });
+
+  return decoded;
+}

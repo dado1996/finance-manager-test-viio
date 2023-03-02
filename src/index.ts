710a8415794c573f07bf3a9687e1644b9f418ce4
Diego Delgado <diegodelgado_96@hotmail.com>
2023-03-02 01:10:13 -0500

    added middlewares for errors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, json } from "express";
 import "dotenv/config";
+import routesApi from "./routes/index";
+import { errorHandler } from "./middlewares/error.handler";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.json({
     hello: 'world',
   })
 });
+
+app.use(json());
+routesApi(app);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Application running on port ${PORT}`);

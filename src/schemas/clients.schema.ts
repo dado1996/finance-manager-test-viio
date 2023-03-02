9ad21e89c5ece6e1c3d3afa2074f1e9ba79c09db
Diego Delgado <diegodelgado_96@hotmail.com>
2023-03-02 03:17:30 -0500

    add edit schemas for updating data

--- src/schemas/clients.schema.ts
+++ src/schemas/clients.schema.ts
@@ -1,10 +1,23 @@
+import e from "express";
 import Joi from "joi";
 
+const id = Joi.number().integer().positive();
 const name = Joi.string();
 const email = Joi.string().email();
 const password = Joi.string().regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&\.])[A-Za-z\d@$!%*#?&\.]{3,}$/);
 
 export const createClientSchema = Joi.object({
+  name: name,
+  email: email.required(),
+  password: password.message('Not a valid password').required(),
+  confirmPassword: password.allow(Joi.ref('password')).message('Passwords must match').required(),
+});
+
+export const editClientSchemaParams = Joi.object({
+  id: id.required(),
+});
+
+export const editClientSchemaBody = Joi.object({
   name: name,
   email: email.required(),
   password: password.message('Not a valid password').required(),

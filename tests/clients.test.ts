9630313d3d0e8f7851f8cf83306e75294ddd7c16
Diego Delgado <diegodelgado_96@hotmail.com>
2023-03-02 12:45:05 -0500

    added jest testing

---
+++ tests/clients.test.ts
@@ -0,0 +1,26 @@
+import ClientsService from "../src/services/clients.services";
+import { describe, expect, jest, test } from "@jest/globals";
+
+const service = new ClientsService();
+
+describe("clients tests", () => {
+  test("get all clients", async () => {
+    
+  });
+
+  test("get a single client", async () => {
+    
+  });
+
+  test("create a client", async () => {
+    
+  });
+
+  test("edit a client", async () => {
+    
+  });
+
+  test("delete a client", async () => {
+    
+  });
+});
